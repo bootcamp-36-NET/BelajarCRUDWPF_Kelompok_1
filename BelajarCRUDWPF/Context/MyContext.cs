cd6eff813eaac60b5d5d0933c8f29e34458f6c35
rahayuputrip <68858800+rahayuputrip@users.noreply.github.com>
2020-08-16 19:36:30 +0700

    Add files via upload

--- BelajarCRUDWPF/Context/MyContext.cs
+++ BelajarCRUDWPF/Context/MyContext.cs
@@ -11,9 +11,42 @@ namespace BelajarCRUDWPF.Context
     class MyContext : DbContext
     {
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionItem> TransactionItems { get; set; }
 
         public MyContext() : base("BelajarCRUDWPF")
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .HasRequired<Supplier>(S => S.Supplier)
+                .WithMany(S => S.Item).HasForeignKey<int>(I => I.SupplierId);
+
+            modelBuilder.Entity<Supplier>()
+                .HasMany<Item>(s => s.Item)
+                .WithRequired(i => i.Supplier)
+                .HasForeignKey<int>(i => i.SupplierId)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<TransactionItem>()
+               .HasRequired<Transaction>(ti => ti.Transaction)
+               .WithMany(ti => ti.TransactionItem).HasForeignKey<int>(t => t.TransactionId);
+
+            modelBuilder.Entity<Transaction>()
+                .HasMany<TransactionItem>(t => t.TransactionItem)
+                .WithRequired(ti => ti.Transaction)
+                .HasForeignKey<int>(ti => ti.TransactionId)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Item>()
+                .HasMany<TransactionItem>(i => i.TransactionItems)
+                .WithRequired(ti => ti.Item)
+                .HasForeignKey<int>(ti => ti.ItemId)
+                .WillCascadeOnDelete();
+
         }
     }
 }
